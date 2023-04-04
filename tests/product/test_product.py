110a18fe4d860c1019d57af61d7bab71be766a4d
Davi Albino <negroalbino_@hotmail.com>
2023-04-04 17:24:50 -0300

    test: test cria produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+                        1,
+                        'Playstation 5',
+                        'Sony',
+                        '20/10/2020',
+                        '20/10/2030',
+                        'NP54',
+                        'Ao abrido de luz solar'
+                    )
+    assert product.id == 1
+    assert product.nome_do_produto == 'Playstation 5'
+    assert product.nome_da_empresa == 'Sony'
+    assert product.data_de_fabricacao == '20/10/2020'
+    assert product.data_de_validade == '20/10/2030'
+    assert product.numero_de_serie == 'NP54'
+    assert product.instrucoes_de_armazenamento == 'Ao abrido de luz solar'
