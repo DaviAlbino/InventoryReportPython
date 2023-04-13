3528a5f867f9c4977fb3b25e67f80b18d0d3b63f
Davi Albino <negroalbino_@hotmail.com>
2023-04-13 01:01:28 -0300

    feat: complete report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,26 @@
-class CompleteReport:
-    pass
+from .simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+
+    def inventory_companies(products):
+        comp_str = ""
+        companies = [product["nome_da_empresa"] for product in products]
+        company_common = Counter(companies)
+        for company, count in company_common.items():
+            comp_str += f"- {company}: {count}\n"
+        return comp_str
+
+    @staticmethod
+    def generate(products: list[dict]):
+        return (
+            f"Data de fabricação mais antiga: "
+            f"{min(CompleteReport.oldest_product_generator(products))}\n"
+            f"Data de validade mais próxima: "
+            f"{CompleteReport.closest_valid_date_generator(products)}\n"
+            f"Empresa com mais produtos: "
+            f"{CompleteReport.most_common_company_generator(products)}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{CompleteReport.inventory_companies(products)}"
+        )
